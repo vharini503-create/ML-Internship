72c84c7d2e35cb0065cdc5bf8a8d85848a528cae
vharini503-create <vharini503@gmail.com>
2026-07-26 12:31:18 +0530

    Added Day-2.py

---
+++ Day-2.py
@@ -0,0 +1,17 @@
+name = "Harsha"
+age = 19
+college = "B.Tech AIML"
+
+print(name)
+print(age)
+print(college)
+
+a = 10
+b = 3.14
+name = "Harsha"
+student = True
+
+print(type(a))
+print(type(b))
+print(type(name))
+print(type(student))
